16ae041d722e797a20bdc3a2baedd4cd3012705d
Fdvky1 <fdvkey@gmail.com>
2023-10-19 21:15:59 +0700

    feat: layout

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <div class="container mx-auto mb-4">
+            <div class="flex justify-between items-center">
+                <div>
+                    <NuxtLink>Nganu Commerce</NuxtLink>
+                </div>
+                <nav class="flex items-center gap-6">
+                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink to="/products">Products</NuxtLink>
+                    <NuxtLink to="/cart">Cart</NuxtLink>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
